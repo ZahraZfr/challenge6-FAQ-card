0ded2fcbf61a140f4f28192e9815836710654965
ZahraZfr <Zahra.Z@m.lo.agency>
2021-08-27 22:56:00 +0430

    fix

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const questions = document.querySelectorAll('.question');
+const questions = document.querySelectorAll(".question");
 questions.forEach((question) => {
-    question.addEventListener("click" , () => {
+    question.addEventListener("click", () => {
         question.classList.toggle("active")
     })
 });
 
 
+
+
+// document.querySelectorAll(".question").forEach((question) => question.addEventlistener("click", () => {
+//     if (question.parentNode.classList.contains("active")) {
+//         question.parentNode.classList.toggle("active")
+//     } else {
+//         document.querySelectorAll(".question").forEach(question => question.parentNode.classList.remove("active"))
+//         question.parentNode.classList.add("active")
+//     }
+// }))
