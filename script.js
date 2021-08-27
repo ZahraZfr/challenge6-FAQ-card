7ecb4cff6937bab3a97121c85f9d43f405d6318b
ZahraZfr <Zahra.Z@m.lo.agency>
2021-08-27 23:52:47 +0430

    fix

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const questions = document.querySelectorAll(".question");
 questions.forEach((question) => {
     question.addEventListener("click", () => {
         question.classList.toggle("active")
+
+
     })
 });
-
 
 
 
